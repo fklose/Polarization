5e1bd76da8008febec64bf9ae34a7ea9738dbc3c
Felix <felix.f.klose@gmail.com>
2022-06-15 15:56:08 -0700

    Added updated version of poisson.py that includes a better performing fitting procedure based on scipy.optimize.minimize()

--- routines/poisson.py
+++ routines/poisson.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.optimize import minimize
+from scipy.optimize import LbfgsInvHessProduct
 
 
 def centralDiff(f, x, p, i, eps):
@@ -18,7 +20,7 @@ def makeVector(f, x, data, p, eps):
     OUT = np.zeros(M)
     
     for i in range(M):
-        OUT[i] = - np.sum((data / f(x, *p) - 1) * centralDiff(f, x, p, i, eps))
+        OUT[i] = np.sum((data / f(x, *p) - 1) * centralDiff(f, x, p, i, eps))
     
     return OUT
 
@@ -31,12 +33,12 @@ def makeMatrix(f, x, data, p, eps):
     
     for i in range(M):
         for j in range(M):
-            OUT[i, j] = - np.sum(data / (f(x, *p)**2) * centralDiff(f, x, p, i, eps) * centralDiff(f, x, p, j, eps))
+            OUT[i, j] = np.sum(data / (f(x, *p)**2) * centralDiff(f, x, p, i, eps) * centralDiff(f, x, p, j, eps))
 
     return OUT
 
 
-def fit(f, x, y, p0, iter=200, weights=None):
+def fit(f, x, y, p0, iter=200):
     
     eps = 0.00001
     
@@ -44,41 +46,86 @@ def fit(f, x, y, p0, iter=200, weights=None):
     N = len(x)
     dof = N - M
     
-    if weights == None:
-        weights = np.ones(N)
-    elif len(weights) != len(x):
-        print("Each point must have a weight!")
-        return
-    else:
-        weights = np.asarray(weights)
-    
     p0 = np.asarray(p0)
     p = np.copy(p0)
     
     for _ in range(iter):
-        p += np.linalg.solve(makeMatrix(f, x, y, p, eps), makeVector(f, x, y, p, eps))
+        B = makeMatrix(f, x, y, p, eps)
+        b = makeVector(f, x, y, p, eps)
+        dp = np.linalg.solve(B, b)
+        p += dp
     
     # Compute Chi-Squared
     X2 = np.zeros(np.shape(y))
+    # To avoid errors X2 is computed first for all points where y != 0 to avoid infinities
+    # Where y = 0 enforce that 0*log(0) = 0 and not inf
     X2[y != 0] = f(x[y != 0], *p) - y[y != 0] + y[y != 0] * np.log(y[y != 0] / f(x[y != 0], *p))
-    # Checking where y == 0 since log(0) = -np.inf but I am enforcing 0 * inf = 0
     X2[y == 0] = f(x[y == 0], *p)
     X2 = 2*np.sum(X2)
     
     # Compute errors
     E1 = np.zeros(M)
-    B_inv = np.linalg.inv(makeMatrix(f, x, y, p, eps))
+    B_inv = np.linalg.inv(B)
     for i in range(M):
         # main diagonal of B_inv looks like its always negative?
         # Fixed by swapping the signs of the elements in b and B
         # Alternatively can also take absolute value
-        E1[i] = np.sqrt(np.abs(B_inv[i,i]))
+        E1[i] = np.sqrt(B_inv[i,i])
     
     E2 = np.zeros(M)
     for i in range(M):
-        E2[i] = E1[i]*np.sqrt(np.sum(weights * (y - f(x, *p))**2) / dof)
+        E2[i] = E1[i]*np.sqrt(np.sum((y - f(x, *p))**2) / dof)
     
     # Compute scaled error (standard deviation)
+    sigma = E2 / np.sqrt(X2 / dof)
+    
+    return p, E1, E2, sigma, X2
+
+
+def altFit(f, x, y, p0, bounds):
+    # Instead of using method outlined in PHYSICA manual use scipy.minimize
+    
+    # Define log-likelihood function
+    LL = lambda p : - np.sum(y * np.log(f(x, *p)) - f(x, *p))
+    
+    # Minimize using scipy.optimize.minimize
+    res = minimize(LL, p0, bounds=bounds)
+    
+    p = res.x
+    
+    # Errors are obtained from the inverse of the Hessian matrix
+    M = len(p)
+    N = len(x)
+    dof = N - M
+    
+    # Try and compute the Hessian and its inverse by hand
+    try:
+        B = makeMatrix(f, x, y, p, 0.00001)
+        B_inv = np.linalg.inv(B)
+    except np.linalg.LinAlgError:
+        B_inv = res.hess_inv
+    
+    if type(B_inv) == LbfgsInvHessProduct:
+        # Extract main diagonal
+        B_inv = B_inv.todense()
+        
+    E1 = np.zeros(M)
+    for i in range(len(p)):
+        E1[i] = np.sqrt(B_inv[i, i])
+    
+    E2 = np.zeros(M)
+    for i in range(M):
+        E2[i] = E1[i]*np.sqrt(np.sum((y - f(x, *p))**2) / dof)
+        
+    # Compute Chi-Squared
+    X2 = np.zeros(np.shape(y))
+    # To avoid errors X2 is computed first for all points where y != 0 to avoid infinities
+    # Where y = 0 enforce that 0*log(0) = 0 and not inf
+    X2[y != 0] = f(x[y != 0], *p) - y[y != 0] + y[y != 0] * np.log(y[y != 0] / f(x[y != 0], *p))
+    X2[y == 0] = f(x[y == 0], *p)
+    X2 = 2*np.sum(X2)
+    
+    # Compute scaled error
     sigma = E2 / np.sqrt(X2 / dof)
     
     return p, E1, E2, sigma, X2
