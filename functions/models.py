9fd74749b389fd3306bc910fb1cbd464750883f3
Felix <felix.f.klose@gmail.com>
2022-06-17 10:11:47 -0700

    Flipped sign in front of AC Stark Shift contribution.

--- functions/models.py
+++ functions/models.py
@@ -46,15 +46,15 @@ def F2_pi_sublevels_stark(x, am2, am1, a0, a1, a2, x0, h, s, g, B, delta, I):
     a1 = np.abs(a1)
     a2 = np.abs(a2)
     
-    F1_ex_m1 = 3*am1*voigt(x, x0 - h + ZeemanShiftF2F1(-1) + ACStarkShiftF2F1(-1), s, g)
-    F1_ex_0 = 4*a0*voigt(x, x0 - h + ZeemanShiftF2F1(0) + ACStarkShiftF2F1(0), s, g)
-    F1_ex_1 = 3*a1*voigt(x, x0 - h + ZeemanShiftF2F1(1) + ACStarkShiftF2F1(1), s, g)
+    F1_ex_m1 = 3*am1*voigt(x, x0 - h + ZeemanShiftF2F1(-1) - ACStarkShiftF2F1(-1), s, g)
+    F1_ex_0 = 4*a0*voigt(x, x0 - h + ZeemanShiftF2F1(0) - ACStarkShiftF2F1(0), s, g)
+    F1_ex_1 = 3*a1*voigt(x, x0 - h + ZeemanShiftF2F1(1) - ACStarkShiftF2F1(1), s, g)
     F1_ex = F1_ex_m1 + F1_ex_0 + F1_ex_1
     
-    F2_ex_m2 = 4*am2*voigt(x, x0 + ZeemanShiftF2F2(-2) + ACStarkShiftF2F2(-2), s, g)
-    F2_ex_m1 = 1*am1*voigt(x, x0 + ZeemanShiftF2F2(-1) + ACStarkShiftF2F2(-1), s, g)
-    F2_ex_1 = 1*a1*voigt(x, x0 + ZeemanShiftF2F2(1) + ACStarkShiftF2F2(1), s, g)
-    F2_ex_2 = 4*a2*voigt(x, x0 + ZeemanShiftF2F2(2) + ACStarkShiftF2F2(2), s, g)
+    F2_ex_m2 = 4*am2*voigt(x, x0 + ZeemanShiftF2F2(-2) - ACStarkShiftF2F2(-2), s, g)
+    F2_ex_m1 = 1*am1*voigt(x, x0 + ZeemanShiftF2F2(-1) - ACStarkShiftF2F2(-1), s, g)
+    F2_ex_1 = 1*a1*voigt(x, x0 + ZeemanShiftF2F2(1) - ACStarkShiftF2F2(1), s, g)
+    F2_ex_2 = 4*a2*voigt(x, x0 + ZeemanShiftF2F2(2) - ACStarkShiftF2F2(2), s, g)
     F2_ex = F2_ex_m2 + F2_ex_m1 + F2_ex_1 + F2_ex_2    
     
     return F1_ex + F2_ex
