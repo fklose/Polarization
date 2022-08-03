b057802a5ff3d4093c3f849a05888f55789c68be
Felix <felix.f.klose@gmail.com>
2022-08-03 15:40:39 -0700

    Moved uncertainty code into routines/uncertainties.py

--- polarization_two uncertainties.py
+++ polarization_two uncertainties.py
@@ -11,6 +11,7 @@ from scipy.optimize import curve_fit
 from scipy.optimize import minimize
 from scipy.optimize import newton
 from itertools import combinations
+from routines.uncertainties import estimateErrorsMonteCarlo
 
 # Define plot styles
 flip_ebar = {"capsize":3, "ls":"", "marker":".", "label":"$OP_{Flip}$"}
@@ -87,185 +88,13 @@ model = lambda x, am2, am1, a0, a1, a2, s: F2_pi_sublevels_FAST(x, am2, am1, a0,
 mle = lambda p, args: - np.sum(args[1] * np.log(model(args[0], *p)) - model(args[0], *p))
 
 # Fit populations
+minimize_kwargs = {"bounds" : [(0, np.inf)]*5 + [(0.01, np.inf)], "tol" : 1e-16}
+
 p0_flip = [1, 1, 1, 1, 1, 1]
 p0_flip, _ = curve_fit(model, x_flip, y_flip, p0=p0_flip)
-res_flip = minimize(mle, p0_flip, args=[x_flip, y_flip], bounds=[(0, np.inf)]*5 + [(1, np.inf)], tol=1e-16)
+res_flip = minimize(mle, p0_flip, args=[x_flip, y_flip], **minimize_kwargs)
 p_flip = res_flip.x
-
-# Compute correlation matrix
-def computeInverseCorrelationMatrix(mle, p, args, eps):
-    
-    matrix = np.zeros(shape=(len(p), len(p)))
-    
-    for i, j in combinations(range(len(p)), 2):
-        if i == j:
-            if p[i] != 0:
-                left = [*p[:i], p[i]*(1 - eps), *p[i+1:]]
-                right = [*p[:i], p[i]*(1 + eps), *p[i+1:]]
-                delta = p[i]*eps
-            else:
-                left = [*p[:i], p[i] - eps, *p[i+1:]]
-                right = [*p[:i], p[i] + eps, *p[i+1:]]
-                delta = eps
-            
-            partial = (mle(left, args) - 2 * mle(p, args) + mle(right, args)) / delta**2
-        
-        else:
-            if p[i] != 0:
-                left = [*p[:i], p[i]*(1 - eps), *p[i+1:]]
-                right = [*p[:i], p[i]*(1 + eps), *p[i+1:]]
-                delta = p[i]*eps
-            else:
-                left = [*p[:i], p[i] - eps, *p[i+1:]]
-                right = [*p[:i], p[i] + eps, *p[i+1:]]
-                delta = eps
-            
-            if p[j] != 0:
-                left0 = [*left[:j], left[j]*(1 - eps), *left[j+1:]]
-                left1 = [*left[:j], left[j]*(1 + eps), *left[j+1:]]
-                right0 = [*right[:j], right[j]*(1 - eps), *right[j+1:]]
-                right1 = [*right[:j], right[j]*(1 + eps), *right[j+1:]]
-                delta *= p[j]*eps
-            else:
-                left0 = [*left[:j], left[j] - eps, *left[j+1:]]
-                left1 = [*left[:j], left[j] + eps, *left[j+1:]]
-                right0 = [*right[:j], right[j] - eps, *right[j+1:]]
-                right1 = [*right[:j], right[j] + eps, *right[j+1:]]
-                delta *= eps
-            
-            partial = (mle(left0, args) + mle(right1, args) - mle(left1, args) - mle(right0, args)) / 4 / delta
-            
-        matrix[i, j] = partial
-        matrix[j, i] = partial
-            
-    return matrix
-
-# TODO !!--FIXED???--!! This does not seem to give the right result. Need to double check
-# TODO How should I pick FDA step size 1e-7, 1e-9, 1e-10 give decreasing uncertainties, starts breaking when too small or too large
-# 1e-8 gives non-sense? Outside of this range matrix is singular and cannot be inverted
-# TODO Scott Oser's notes mention that this is valid in the Gaussian approximation
-# (does this mean Gaussian wrt to error distribution or something else)
-# m = computeInverseCorrelationMatrix(mle, p_flip, (x_flip, y_flip), 1e-7)
-
-# m_inv = np.linalg.inv(m)
-
-# print("Inverse Covariance Matrix")
-# print('\n'.join(['\t'.join(['{:.3e}'.format(item) for item in row]) 
-#       for row in m_inv]))
-
-# # print(np.round(np.linalg.inv(m), 3))
-
-# # m_inv = np.linalg.inv(m)
-
-# print("Parameter Uncertainties")
-# for i in range(len(p_flip)):
-#     print(np.round(p_flip[i], 3), end="\t+/- ")
-#     print(np.round(np.sqrt(np.abs(m_inv[i, i])), 4))
-    
-
-def estimateErrorsMonteCarlo(mle, popt, args, x, y, N):
-    """Estimate errors by simulating experiment.
-    Experiemnts are simulated by drawing points from a dataset with replacement
-    Each simulation will sample the same number of data points as the data.
-    Each simulated experiment will be fitted using the model and the resulting variance in fit
-    parameters is proportional to the error on the fit parameter.
-    
-    See 15.6.1 of Numerical Recipes by Press, Teukolsky and Vetterling
-    """
-    
-    # 'Unpack' histogram
-    samples = []
-    for occurances, value in zip(y, x):
-        samples += [value] * occurances
-    
-    # Simulate experiment N times, fit it and save parameters
-    params = []
-    for h in range(N):
-        data = np.random.choice(samples, size=sum(y), replace=True)
-        experiment = [0]*len(x)
-        for i, v in enumerate(x):
-            experiment[i] += sum(data == v)
-        
-        # Fit mle to experiment and save parameters
-        params.append(minimize(mle, popt, [x, experiment], bounds=[(-np.inf, np.inf)]*5 + [(0.1, np.inf)], tol=1e-16).x)
-        
-        print(h)
-    
-    return np.asarray(params)
-
-params = estimateErrorsMonteCarlo(mle, p_flip, [x_flip, y_flip], x_flip, y_flip, 100)
-
-fig, axes = plt.subplots(2, 3)
-axes[0, 0].hist(params[:, 0], bins=20)
-axes[0, 1].hist(params[:, 1], bins=20)
-axes[0, 2].hist(params[:, 2], bins=20)
-axes[1, 0].hist(params[:, 3], bins=20)
-axes[1, 1].hist(params[:, 4], bins=20)
-axes[1, 2].hist(params[:, 5], bins=20)
-plt.show()
-
-print(np.round(params, 3))
-
-table = []
-for i in range(len(p_flip)):
-    table.append([p_flip[i], np.std(params[:, i])])
-
-print("Parameter Uncertainties from Monte Carlo")
-print(tabulate(table))
-
-
-
-# def estimateErrors(mle, popt, args):
-#     """Determines 1-sigma parameter errors by finding parameter values to produce Delta ln(fun) = ln(fun(popt)) - ln(fun(p)) = 1/2
-    
-#     See Lecture 8 of Scott Oser's PHYS509 slides starting on slide 18
-#     https://phas.ubc.ca/~oser/p509/Lec_08.pdf
-#     """
-    
-#     # target = mle(popt, args) + 1/2
-#     target = mle(popt, args) + 7.04 / 2
-    
-#     lower_errors = []
-#     upper_errors = []
-#     for i, p0 in enumerate(popt):
-#         print(i)
-#         # Make lambda
-#         p = lambda x: [*popt[:i], p0 + x, *popt[i+1:]]
-#         fun = lambda x: mle(p(x), args)
-        
-#         if p0 == 0:
-#             X = np.linspace(-1, 1, 1000)
-#         elif i == 5:
-#             X = np.linspace(0, 2, 1000)
-#         else:
-#             X = np.linspace(-p0 / 2, p0 / 2, 1000)
-#         plt.plot(X + p0, [fun(x) - target for x in X])
-#         # plt.ylim(-1, 1)
-#         plt.vlines(p0, -3, 3)
-#         plt.show()
-        
-#         # Obtain initial guess for Newton's method
-#         ub = 0
-#         # while np.abs(fun(ub) - target) > 1e-3:
-#         #     ub += 1e-3
-
-#         lb = 0
-#         # while np.abs(fun(lb) - target) > 1e-3:
-#         #     lb -= 1e-3
-
-        
-        
-#         ub = newton(fun, ub)
-#         lb = newton(fun, lb)
-        
-#         lower_errors.append(lb)
-#         upper_errors.append(ub)
-    
-#     return lower_errors, upper_errors
-
-
-# print(*estimateErrors(mle, p_flip, (x_flip, y_flip)))
-
+simulated_params = estimateErrorsMonteCarlo(mle, p_flip, x_flip, y_flip, 100, minimize_kwargs)
 
 # p0_norm = [1, 1, 1, 1, 1, 1]
 # p0_norm, _ = curve_fit(model, x_norm, y_norm, p0=p0_norm)
