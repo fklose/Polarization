3fde490f3435c0239c135120c1450e5a2d45285e
Felix <felix.f.klose@gmail.com>
2022-12-29 16:54:44 -0500

    Added more comments

--- polarization.py
+++ polarization.py
@@ -53,16 +53,24 @@ SPECTRUM_BITS_NORM = data_norm["BITS"][
     &   ((CUTS["TTTL_OP_Beam"][0] < data_norm["TTTL_OP_Beam"]) & (data_norm["TTTL_OP_Beam"] < CUTS["TTTL_OP_Beam"][1]))
 ]
 
-# Using VCO calibration
-V = np.linspace(V_MIN, V_MAX, STEPS)
+# Obtain scan frequencies using measured VCO voltage
+# Note that the VCO voltage-frequency curves are not the same for sweeping up vs sweeping down
 VCO_V_down, VCO_f_down = np.loadtxt("./AOM Calibrations/M1212-aQ50-2/calibration_downsweep.csv", unpack=True)
 VCO_V_up  , VCO_f_up   = np.loadtxt("./AOM Calibrations/M1212-aQ50-2/calibration_upsweep.csv", unpack=True)
+
+# DAQ does not now about stepping up or down, so average up- and downsweep together
 VCO_V = (np.flip(VCO_V_down) + VCO_V_up) / 2
 VCO_f = (np.flip(VCO_f_down) + VCO_f_up) / 2
 
+# Compute scan voltages using measured max and minimum scan voltage
+# This assumes an even number of steps
+V = np.linspace(V_MIN, V_MAX, STEPS)
+
+# Transform DAQ QDC_EIO bits into frequencies i.e. convert data from steps to frequency
 unbinned_flip = 2*np.interp(V[SPECTRUM_BITS_FLIP], VCO_V, VCO_f) + LOCKPOINT
 unbinned_norm = 2*np.interp(V[SPECTRUM_BITS_NORM], VCO_V, VCO_f) + LOCKPOINT
 
+# Obtain frequency steps over scan range
 frequencies = 2*np.interp(V, VCO_V, VCO_f) + LOCKPOINT
 
 # Generate bins by selecting midpoints between adjacent frequencies and bin data
@@ -84,7 +92,7 @@ def global_poisson_likelihood(x0, h, B, s, g, am2_norm, am1_norm, a0_norm, ap1_n
     
     return - np.sum(y_norm * np.log(norm) - norm) - np.sum(y_flip * np.log(flip) - flip)
 
-# Make the minimizer object
+# Initialize the minimizer object, assigning the cost_function and an initial guess
 m = Minuit(global_poisson_likelihood, x0=363, h=9.94, B=-2.1, s=10, g=1.1,
            am2_flip=1, am1_flip=1, a0_flip=1, ap1_flip=1, ap2_flip=10,
            am2_norm=10, am1_norm=1, a0_norm=1, ap1_norm=1, ap2_norm=1)
